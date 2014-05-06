d5a6b177418dbb3dd9885589a40a12514e02c3b3
Leslie Snively <les@windstream.biz>
2014-05-06 16:18:00 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocationEntry::Application.config.secret_key_base = '230e34ac9989b409a9cb4607cdc44c8ddc6fc8b8df61f079138ad064fbaaceee24b84a56d81c5c8f4f68cf1056f85e84ca15b0c062ff9dfa9ef020e538f9c1ad'
